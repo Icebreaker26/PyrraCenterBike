a9e6a86be53e00d65e79b6203145b87eef48d6b1
Ice <alejandro.torres0826@gmail.com>
2021-07-06 22:22:20 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('a[href^="#"]').click(function() {
+      var destino = $(this.hash); //this.hash lee el atributo href de este
+      $('html, body').animate({ scrollTop: destino.offset().top }, 700); //Llega a su destino con el tiempo deseado
+      return false;
+    });
+  });
+
